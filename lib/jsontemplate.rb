053617613a4dad7c219da377790b8272dbdc2f66
Vidar Hokstad <vidar@hokstad.com>
2016-07-14 05:18:49 +0100

    Initial version of code

--- lib/jsontemplate.rb
+++ lib/jsontemplate.rb
@@ -1,5 +1,40 @@
+require 'json'
 require "jsontemplate/version"
 
-module JsonTemplate
-  # Your code goes here...
+class JsonTemplate
+    def initialize(path)
+        @path = path
+        @dir  = File.dirname(path)
+        @json = JSON.load(File.read(path))
+    end
+
+    def process_dirmerge(pattern)
+        files = Dir.glob(@dir+"/"+pattern)
+        files.collect do |path|
+            r = JsonTemplate.new(path).process_dict
+            key=File.basename(path).split(".")[0..-2].join(".")
+            [key, r]
+        end.flatten(1)
+    end
+
+    def process_dict(root=nil)
+        root ||= @json
+
+        pairs = root.collect do |k,v|
+            if v.is_a?(Hash)
+                v = process_dict(v)
+            end
+
+            if k == "$DirMerge"
+                process_dirmerge(v)
+            elsif k[0] == "$"
+                raise "Unknown directive #{k}"
+            else
+                [[k,v]]
+            end
+        end
+        Hash[*pairs.flatten(2)]
+    end
 end
+
+
--- lib/jsontemplate/version.rb
+++ lib/jsontemplate/version.rb
@@ -1,3 +1,3 @@
-module JsonTemplate
+class JsonTemplate
   VERSION = "0.1.0"
 end
