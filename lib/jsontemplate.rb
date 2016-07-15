3abd656e1325fe4347f70150d52e0955856091f3
Vidar Hokstad <vidar@hokstad.com>
2016-07-15 14:31:56 +0100

    Refactoring initialization to make it more test friendly; Added basic tests; Added FLookup directive

--- lib/jsontemplate.rb
+++ lib/jsontemplate.rb
@@ -2,19 +2,29 @@ require 'json'
 require "jsontemplate/version"
 
 class JsonTemplate
-    def initialize(path)
-        @path = path
-        @dir  = File.dirname(path)
-        @json = JSON.load(File.read(path))
+    attr_reader :json
+
+    def initialize(src, path=nil)
+        path ||= src.kind_of?(String) ? src : nil
+        @path = path || (src.respond_to?(:path) ? src.path : nil)
+        @dir  = File.expand_path(path ? File.dirname(path) : ".")
+
+        data = src.respond_to?(:read) ? src.read : File.read(src)
+        @json = JSON.load(data)
     end
 
     def process_dirmerge(pattern)
         files = Dir.glob(@dir+"/"+pattern)
         files.collect do |path|
-            r = JsonTemplate.new(path).process_dict
+            r = JsonTemplate.new(path,@path).process_dict
             key=File.basename(path).split(".")[0..-2].join(".")
             [key, r]
         end.flatten(1)
+    end
+
+    def process_flookup(file, v)
+        map = JsonTemplate.new(file,@path).process_dict
+        map[v]
     end
 
     def process_dict(root=nil)
@@ -27,13 +37,19 @@ class JsonTemplate
 
             if k == "$DirMerge"
                 process_dirmerge(v)
+            elsif k == "$FLookup"
+                process_flookup(*v)
             elsif k[0] == "$"
                 raise "Unknown directive #{k}"
             else
-                [[k,v]]
+                [k,v]
             end
         end
-        Hash[*pairs.flatten(2)]
+        if pairs.length == 1 && !pairs.first.kind_of?(Array)
+            pairs.first
+        else
+            Hash[*pairs.flatten(1)]
+        end
     end
 end
 
--- spec/jsontemplate_spec.rb
+++ spec/jsontemplate_spec.rb
@@ -1,11 +1,17 @@
 require 'spec_helper'
 
 describe JsonTemplate do
+  let (:basicjson) { '{"foo": "bar"}' }
+
   it 'has a version number' do
     expect(JsonTemplate::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  it 'returns an identical JSON structure if passed a simple JSON document with no directives' do
+    expect(JsonTemplate.new(StringIO.new(basicjson)).process_dict).to eq(JSON.load(basicjson))
+  end
+
+  it 'allows key lookup and expansion from another JSON file' do
+    expect(JsonTemplate.new(StringIO.new('{"foo": {"$FLookup": ["parameters.json","key1"]}}')).process_dict).to eq({"foo" => "value1"})
   end
 end
